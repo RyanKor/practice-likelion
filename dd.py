c9846b8e23330db37f0017d8fe29a3553ea6e812
chaeyeonie <chaeyeoni345@gamil.com>
2019-07-15 21:16:26 +0900

    chaeyeon

--- dd.py
+++ dd.py
@@ -1,1 +1,8 @@
-hi
+ elif user == "보":
+        if random.choice(["가위", "바위", "보"]) == "가위":
+            print("패배했습니다")
+        elif random.choice(["가위", "바위", "보"]) == "주먹":
+            print("승리했습니다.")
+            game = False
+        else:
+            print("비겼습니다.")
