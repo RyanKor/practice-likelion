3b9667deb2ee7feb5b32a603d657a42156bd8e9d
Sonia <syparkehsc@naver.com>
2019-07-15 21:13:43 +0900

    가위

--- dd.py
+++ dd.py
@@ -1,3 +1,8 @@
-def chunjae:
-  print('chae-yeon')
-  return 'bu-rup-dda'
+if user == "가위":
+    if random.choice(["가위", "바위", "보"]) == "보":
+        print("승리했습니다")
+        game = False
+    elif random.choice(["가위", "바위", "보"]) == "바위":
+        print("패배했습니다.")
+    else:
+        print("비겼습니다.")
