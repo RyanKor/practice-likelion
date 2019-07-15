d0408843c580afff0cc542fddaf351ea01db5f24
antlikelion <ignorant_woo@naver.com>
2019-07-15 21:14:26 +0900

    가위바위보 인우

--- dd.py
+++ dd.py
@@ -1,13 +1,10 @@
-
-def chunjae:
-  print('chae-yeon')
-  return 'bu-rup-dda'
-
-def inwoo(a, b):
-    return a + b
-
-import random
-
-def ShFunction(a,b,c):
-  c= a + b 
-  return c
+# 인우
+elif user == "바위":
+    if random.choice(["가위", "바위", "보"]) == "보":
+        print("패배했습니다")
+    elif random.choice(["가위", "바위", "보"]) == "가위":
+        print("승리했습니다.")
+        game = False
+    else:
+        print("비겼습니다.")
+# 인우
